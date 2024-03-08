415dfa759dc91617af3bb12222fa1091781ed471
MichelaVivacqua <vivacquamichela@gmail.com>
2024-03-08 11:50:01 +0100

    implementazione di video funzionante

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import entities.Audio;
+import entities.Video;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +8,32 @@ public class Main {
         Audio nonDirgliMai = new Audio("Non dirgli mai", 5, 3);
 
 nonDirgliMai.play();
+
+System.out.println("---------------");
+nonDirgliMai.abbassaVolume();
+nonDirgliMai.play();
+        System.out.println("---------------");
+        nonDirgliMai.alzaVolume();
+        nonDirgliMai.alzaVolume();
+        nonDirgliMai.play();
+
+        Video spotPubblicitario = new Video("Spot pubblicitario Coca Cola", 2, 2,3);
+
+        spotPubblicitario.play();
+
+        System.out.println("---------------");
+       spotPubblicitario.abbassaVolume();
+        spotPubblicitario.play();
+        System.out.println("---------------");
+        spotPubblicitario.alzaVolume();
+        spotPubblicitario.alzaVolume();
+        spotPubblicitario.alzaVolume();
+        spotPubblicitario.play();
+        System.out.println("---------------");
+        spotPubblicitario.diminuisciLuminosita();
+        spotPubblicitario.play();
+        System.out.println("---------------");
+        spotPubblicitario.aumentaLuminosita();
+        spotPubblicitario.play();
     }
 }
--- src/entities/Video.java
+++ src/entities/Video.java
@@ -1,4 +1,76 @@
 package entities;
 
-public class Video {
+import interfaces.PlayAndVol;
+
+public class Video extends ElementoMultimediale implements PlayAndVol {
+    private int volume;
+    private int durata;
+    private int luminosita;
+
+    public Video(String titolo, int volume, int durata, int luminosita) {
+        super(titolo);
+        this.volume = volume;
+        this.durata = durata;
+        this.luminosita=luminosita;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public void setDurata(int durata) {
+        this.durata = durata;
+    }
+
+
+    @Override
+    public void abbassaVolume() {
+        if (volume > 0) {
+            volume--;
+        }
+    }
+
+    @Override
+    public void alzaVolume() {
+        if (volume < 10) {
+            volume++;
+        }
+    }
+
+
+    @Override
+    public void play() {
+        String titoloConVolumeLuminosita = getTitolo();
+        for (int i = 0; i < volume; i++) {
+            titoloConVolumeLuminosita += "!";
+        }
+        for (int i = 0; i < luminosita; i++) {
+            titoloConVolumeLuminosita += "*";
+        }
+        for (int i = 0; i < durata; i++) {
+            System.out.println("Playing: " + titoloConVolumeLuminosita);
+        }
+    }
+
+    public void diminuisciLuminosita() {
+        if (luminosita > 0) {
+           luminosita--;
+        }
+    }
+
+
+    public void aumentaLuminosita() {
+        if (luminosita < 10) {
+            luminosita++;
+        }
+    }
+
 }
